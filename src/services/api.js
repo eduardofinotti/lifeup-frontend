3223a34371025af54528e7c3f9137fe8538a4cc5
Eduardo Finotti <eduardo-finotti@ifood.com.br>
2020-04-12 16:53:27 -0300

    lifeup app

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.169.104:3030'
+    // baseURL: 'http://192.168.169.104:3030'
+    baseURL: 'https://lifeup-backend.herokuapp.com/'
 })
 
 export default api
